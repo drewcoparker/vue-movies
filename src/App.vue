1e80025d6fa3a50e172cf915ab854b415d5d21f7
Drew Parker <drew.i.parker@gmail.com>
2018-04-08 12:55:48 -0400

    Structured out a base movie component that accepts data as props from App, its parent

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+        <h1 style="text-align:center;">Various Movies</h1>
+
+        <movie class="tile"
+            v-for="movie in movies"
+            v-bind:key="movie.id"
+            v-bind:title="movie.title"
+            v-bind:rating="movie.rating"
+            v-bind:description="movie.description"
+            v-bind:director="movie.director" >
+        </movie>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Movie from "./components/Movie.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+
+    components: {
+        Movie
+    },
+
+    data: function() {
+        return {
+            movies: [
+                { id: 1, title: 'Pulp Fiction', rating: 9.5, description: 'A greate film', director: 'Quentin Tarrintino' },
+                { id: 2, title: 'Casino', rating: 7.5, description: 'A violent film', director: 'Martin Scorcese' },
+                { id: 3, title: 'Platoon', rating: 8.5, description: 'A war film', director: 'Oliver Stone' }
+            ]
+        }
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* text-align: center; */
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+.tile {
+    display: inline-block;
+    padding: 1.5rem;
 }
 </style>
---
+++ src/components/Movie.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <p>{{ description }}</p>
+        <p>Directed by: {{ director }}</p>
+        <p>Avg rating: {{ rating }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: {
+        title: String,
+        rating: Number,
+        description: String,
+        director: String
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
